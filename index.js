f2427ed9312fee11f6a4c7a04ce4d1a7ff3c59b0
farvez <farvezanzam03@gmail.com>
2025-07-10 23:36:33 +0530

    intial to-do setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+let todos = [];
+
+app.get('/todos', (req, res) => {
+  res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+  const todo = req.body;
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
